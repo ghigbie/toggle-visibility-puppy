2e110b8ed8d4c8ca13b757f866350c7b3d00fbb2
George Higbie <gehigbie@gmail.com>
2017-12-28 21:04:43 +0000

    Add some basic JSX

---
+++ src/app.js
@@ -0,0 +1,14 @@
+
+
+
+const template = (
+    <div>
+        <h1 className="center">Visibity Toggle</h1>
+        <button className="center">Hide Details</button>
+        <div id="secret"
+             className="center">
+            <h3>The secret you will see in his eyes below</h3>
+            <img src="https://c1.staticflickr.com/5/4058/4577136658_1b58bb02d9_z.jpg"/>
+        </div>
+    </div>
+);
