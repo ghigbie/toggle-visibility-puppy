6f4662f4478ee820c04447284089e594c64befe2
George Higbie <gehigbie@gmail.com>
2018-01-06 05:14:11 +0000

    Remove comments

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -77,34 +77,3 @@ var renderApp = function renderApp() {
 };
 
 renderApp();
-
-// let visibility = false;
-
-// const onToggleVisibility = () => {
-//     visibility = !visibility;
-//     renderReactElements();
-// };
-
-// const renderReactElements = () => {
-//     const appRoot = document.getElementById('react-container');
-//     const template = (
-//         <div>
-//             <h1 className="center">Visibity Toggle</h1>
-//             <button id="toggleDetails"
-//                     className="center btn btn-default"
-//                     onClick={onToggleVisibility}>
-//                     {visibility ? 'Hide Details' : 'Show Details'}
-//             </button>
-//             <h3 className="center">The secret you will see in his eyes below:</h3>
-//             {visibility && (
-//                 <div id="secret"
-//                      className="center">
-//                     <img className="img-thumbnail"
-//                          src="./assets/bulldog.jpg"/>
-//                 </div>)}
-//         </div>
-//     );
-//     ReactDOM.render(template, appRoot);
-// };
-
-// renderReactElements();
